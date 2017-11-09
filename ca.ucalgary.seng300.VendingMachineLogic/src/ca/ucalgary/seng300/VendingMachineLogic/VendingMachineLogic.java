f2d41ba656bb467515550c15ff60e7a03220102d
Jack <jacktuffx@gmail.com>
2017-11-08 19:09:57 -0700

    Add files via upload
    Added initial java project with source files from modified group assignment 1

---
+++ ca.ucalgary.seng300.VendingMachineLogic/src/ca/ucalgary/seng300/VendingMachineLogic/VendingMachineLogic.java
@@ -0,0 +1,144 @@
+/****************************************************
+ * SENG 300 Group Assignment 2
+ * This class handles all the logic operations that occur in a vending machine,
+ * specifically, this class handles the calling of methods when money is inserted
+ * and buttons are pressed as well as the listening of their respective events
+ ****************************************************/
+package ca.ucalgary.seng300.VendingMachineLogic;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import org.lsmr.vending.*;
+import org.lsmr.vending.hardware.*;
+
+public class VendingMachineLogic implements CoinSlotListener, PopCanRackListener, PushButtonListener {
+
+	private VendingMachine vm;
+	private int userCredit;
+	private List<PushButton> buttonList = new ArrayList<>();
+	private String event;
+	
+	/********************
+	 * Constructor
+	 * instantiates and initializes all the relevant hardware
+	 * and registers listeners
+	 *******************/
+	public VendingMachineLogic(VendingMachine vm) {
+		
+		this.vm = vm;
+		userCredit = 0;
+		//For loop to iterate through all the available buttons
+		for (int i = 0; i < vm.getNumberOfSelectionButtons(); i++) {
+			PushButton sb = vm.getSelectionButton(i); //Instantiates the hardware
+			sb.register(this); //And registers the relevant listeners
+			buttonList.add(sb); //Stores into an ArrayList for later use
+		}
+		//Iterate through all available pop can racks
+		for (int i = 0; i < vm.getNumberOfPopCanRacks(); i++) {
+			PopCanRack pcr = vm.getPopCanRack(i); //Instantiates the hardware
+			pcr.register(this); //Registers the relevant listeners
+		}
+		
+		vm.getCoinSlot().register(this);
+				
+	}
+	
+	
+	/**
+	 * @return the current event
+	 */
+	public String getEvent() {
+		return event;
+	}
+	/**
+	 * 
+	 * @return how much credit is in vending machine
+	 */
+	public int getCredit() {
+		return userCredit;
+	}
+	
+	@Override
+	public void enabled(AbstractHardware<? extends AbstractHardwareListener> hardware) {
+		//Leave Empty
+	}
+
+	@Override
+	public void disabled(AbstractHardware<? extends AbstractHardwareListener> hardware) {
+		//Leave Empty		
+	}
+	
+	/*************************
+	 * Handles the logic of selection buttons
+	 *************************/
+	@Override
+	public void pressed(PushButton button) {
+		
+		int btnIndex = buttonList.indexOf(button) ; //Which button did the user press
+		
+		if (btnIndex == -1) { //Unregistered button is pressed
+			//Nothing happens for now
+		}
+		
+		int cost = vm.getPopKindCost(btnIndex);
+		
+		if (cost > userCredit) { //Not enough money!!!
+			//Nothing happens for now
+		} else {
+			PopCanRack pr = vm.getPopCanRack(btnIndex); //Matches the button with the pop rack
+			try {
+				pr.dispensePopCan(); //Dispenses the relevant pop
+				vm.getCoinReceptacle().storeCoins(); //Stores the change
+				userCredit -= cost; //Deduct the pay from the available credit
+			} catch (DisabledException | CapacityExceededException e) {
+				throw new SimulationException(e);
+			} catch (EmptyException e2) {
+				event = "Pop is sold out!"; //Set the event for sold-out
+			}
+		}
+	}
+
+	@Override
+	public void popCanAdded(PopCanRack popCanRack, PopCan popCan) {
+		event = "Added " + popCan.getName();
+	}
+
+	@Override
+	public void popCanRemoved(PopCanRack popCanRack, PopCan popCan) {
+		event = "Removed a " + popCan.getName();
+	}
+
+	@Override
+	public void popCansLoaded(PopCanRack rack, PopCan... popCans) {
+		//Leave empty
+	}
+
+	@Override
+	public void popCansUnloaded(PopCanRack rack, PopCan... popCans) {
+		//Leave empty
+	}
+	
+	@Override
+	public void popCansFull(PopCanRack popCanRack) {
+		//Leave empty		
+	}
+
+	@Override
+	public void popCansEmpty(PopCanRack popCanRack) {
+		//Leave empty
+	}
+	
+	@Override
+	public void validCoinInserted(CoinSlot slot, Coin coin) {
+		userCredit += coin.getValue(); //Increment the credit when valid coins are inserted
+		event = "Inserted $" + coin.getValue();
+	}
+
+	@Override
+	public void coinRejected(CoinSlot slot, Coin coin) {
+		event = "Invalid coin inserted";
+	}
+
+	
+}
---
+++ ca.ucalgary.seng300.VendingMachineLogic/src/ca/ucalgary/seng300/test/Test1.java
@@ -0,0 +1,121 @@
+/****************************************************
+ * SENG 300 Group Assignment 1
+ * Authors: 
+ * Xin Yan (Jack) Xie
+ * Xiangyu (Michael) Han
+ * Zachary Metz
+ * This class tests the functionality of the VendingMachineLogic class
+ * by simulating different possible user interactions
+ * An overall 86.2% code coverage is reached
+ ****************************************************/
+package ca.ucalgary.seng300.test;
+
+import static org.junit.Assert.*;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import org.junit.Before;
+import org.junit.Test;
+
+import ca.ucalgary.seng300.VendingMachineLogic.*;
+import org.lsmr.vending.Coin;
+import org.lsmr.vending.hardware.DisabledException;
+import org.lsmr.vending.hardware.SimulationException;
+import org.lsmr.vending.hardware.VendingMachine;
+
+public class Test1 {
+	
+	private VendingMachine vm;
+	private VendingMachineLogic vml;
+	@Before
+	public void setUp() throws Exception {
+		
+		int[] coinKinds = {5, 10, 25, 100, 200}; //Nickels, dimes, quarters, loonies, toonies (all values in cents)
+		int btnCount = 6; //6 types of pop
+		int coinRackCapacity = 15;
+		int popRackCapacity = 10;
+		int receptacleCapacity = 200;
+		int deliveryChuteCapacity = 10;
+		int coinReturnCapacity = 10;
+		
+		vm = new VendingMachine(coinKinds, btnCount, coinRackCapacity, popRackCapacity, receptacleCapacity, deliveryChuteCapacity, coinReturnCapacity);
+		vml = new VendingMachineLogic(vm);
+		
+		List<String> popNames = new ArrayList<String>(); //List of pop names
+				
+		popNames.add("Water");
+		popNames.add("Pepsi");
+		popNames.add("Sprite");
+		popNames.add("Mountain Dew");
+		popNames.add("Orange Crush");
+		popNames.add("Gatorade");
+		
+		List<Integer> costs = new ArrayList<Integer>(); //List of pop costs
+		
+		for (int i = 0; i < popNames.size(); i++ ) {
+			costs.add(250); //everything costs 2.50
+		}
+		
+		vm.configure(popNames, costs);
+		vm.loadPopCans(5, 5, 5, 5, 5, 5); //Starts with 5 of each kind of pop
+	}
+
+	@Test
+	public void buyPopTest() throws DisabledException {
+		
+		Coin loonie = new Coin(100);
+		Coin toonie = new Coin(200);
+		
+		vm.getCoinSlot().addCoin(loonie); //100
+		assertEquals("Inserted $100", vml.getEvent()); //Tests coin insertion event
+		vm.getCoinSlot().addCoin(toonie); //100 + 200 = 
+		assertEquals(300, vml.getCredit()); //300 cents
+		
+		vm.getSelectionButton(0).press(); //I want water
+		assertEquals("Removed a Water", vml.getEvent()); //Tests pop removal event
+		
+		assertEquals(50, vml.getCredit()); //300 - cost of water (250) = 50 cents
+	}
+	
+	@Test
+	public void soldOutTest() throws DisabledException, SimulationException {
+		Coin loonie = new Coin(100);
+		Coin toonie = new Coin(200);
+		
+		vm.getCoinSlot().addCoin(toonie); //200*7 = 1400
+		vm.getCoinSlot().addCoin(toonie);
+		vm.getCoinSlot().addCoin(toonie);
+		vm.getCoinSlot().addCoin(toonie);
+		vm.getCoinSlot().addCoin(toonie);
+		vm.getCoinSlot().addCoin(toonie);
+		vm.getCoinSlot().addCoin(toonie);
+		vm.getCoinSlot().addCoin(loonie); // 1400 + 100 = 1500
+		assertEquals(1500, vml.getCredit()); //1500 cents
+		
+		vm.getSelectionButton(0).press(); //I want water
+		vm.getSelectionButton(0).press(); //I want water
+		vm.getSelectionButton(0).press(); //I want water
+		vm.getSelectionButton(0).press(); //I want water
+		vm.getSelectionButton(0).press(); //I want water
+		assertEquals("Removed a Water", vml.getEvent()); //Tests pop removal event
+		assertEquals(250, vml.getCredit()); //1500 - 250*5 = 250
+		vm.getSelectionButton(0).press(); //Since the popRack has only 5 bottles,
+		assertEquals("Pop is sold out!", vml.getEvent()); //the pop is already sold out
+		
+		assertEquals(250, vml.getCredit()); //Since the pop is sold out, no charge will occur
+	}
+
+	@Test
+	public void maliciousTest() throws DisabledException {
+		
+		Coin washer = new Coin(1); //Invalid coin
+		
+		vm.getCoinSlot().addCoin(washer);
+		assertEquals("Invalid coin inserted", vml.getEvent());
+		
+		vm.getSelectionButton(2).press(); //I want Sprite for free
+		
+		assertEquals(0, vml.getCredit()); //No credit
+	}	
+}
